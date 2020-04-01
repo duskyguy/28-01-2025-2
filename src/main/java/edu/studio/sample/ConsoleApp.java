36e700a45b07f7e826b093dccfacb45dc11deab8
joyatee <55283088+joyatee@users.noreply.github.com>
2020-04-01 19:33:54 -0400

    POLL and PUSH feature testing

--- src/main/java/edu/studio/sample/ConsoleApp.java
+++ src/main/java/edu/studio/sample/ConsoleApp.java
@@ -3,8 +3,8 @@ package edu.studio.sample;
 public class ConsoleApp {
 
     public static void main(String[] args) {
-        
-        System.out.println("Hello Jenkins");
+        //Changes made for testing Jenkins POLL and PUSH features
+        System.out.println("Hello Jenkins!Testing Poll and Push");
         
     }
 
