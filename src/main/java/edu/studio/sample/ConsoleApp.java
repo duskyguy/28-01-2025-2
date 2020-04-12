b0861218606cdb0d61955d903b6e230e25f1414f
sjamboor <55299060+sjamboor@users.noreply.github.com>
2020-04-11 21:46:01 -0400

    Update ConsoleApp.java

--- src/main/java/edu/studio/sample/ConsoleApp.java
+++ src/main/java/edu/studio/sample/ConsoleApp.java
@@ -5,6 +5,7 @@ public class ConsoleApp {
     public static void main(String[] args) {
         
         System.out.println("Hello Jenkins");
+        System.out.println("Hello from Supriya");
         
     }
 
